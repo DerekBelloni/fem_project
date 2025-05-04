a0550f6fbb097d7b0a0ddd1ac23fcf0e4d7e665d
Derek Belloni <dbelloni@compunet.biz>
2025-05-03 18:40:20 -0600

    created api and store directories, built endpoints for get by id and create

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,16 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/DerekBelloni/fem_project/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 	return app, nil
 }
